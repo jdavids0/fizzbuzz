a3a24d1f693f6e401a108486f6ec2140bf4c8e31
jdavids0 <jbrdavidson@gmail.com>
2022-03-25 16:14:22 -0600

    uploaded fizzbuzz

---
+++ script.js
@@ -0,0 +1,13 @@
+for (i=1; i<101; i++) { // a for loop that iterates over a variable i the increments up to 100
+    if (i % 15 == 0)
+    console.log("Fizzbuzz");
+    else if (i % 5 == 0)
+    console.log("Buzz");
+    else if (i % 3 == 0)
+    console.log ("Fizz");
+    else 
+    console.log (i);
+}
+console.log(i)
+
+// check 15 first because all numbers divisible by 15 will also be divisible by 5 and 3 bc they're divisors of factors of 15
